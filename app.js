5c4fabd42e12d1395a891e9a0cd0d48076c99e3e
Berkyla <118302296+Berkyla@users.noreply.github.com>
2024-05-18 00:56:35 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ document.getElementById('upload-form').addEventListener('submit', function(event
     const fileInput = document.getElementById('file-input');
     if (fileInput.files.length > 0) {
         const file = fileInput.files[0];
-        // Здесь вы можете реализовать логику для загрузки файла на сервер и обработки аудиозаписи
+        
         console.log('Файл загружен:', file);
-        // Пример вывода расшифровки
+        
         document.getElementById('transcript').value = "Пример расшифровки текста для загруженного файла.";
     } else {
         alert('Пожалуйста, выберите файл.');
